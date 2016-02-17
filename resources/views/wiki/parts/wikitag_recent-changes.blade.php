07a93ca1430b2d4cf627ec1297914da1c3afe606
koojunho <koojh@9cells.com>
2016-02-18 05:22:58 +0900

    의미없는 값 지움

--- resources/views/wiki/parts/wikitag_recent-changes.blade.php
+++ resources/views/wiki/parts/wikitag_recent-changes.blade.php
@@ -5,7 +5,6 @@ $hs = WikiHistory::orderBy('id', 'desc')->paginate(10);
 <table class="table table-bordered">
     <thead>
     <tr>
-        <th>#</th>
         <th>제목</th>
         <th>변경</th>
         <th>변경한 사람</th>
@@ -14,7 +13,6 @@ $hs = WikiHistory::orderBy('id', 'desc')->paginate(10);
     <tbody>
     @foreach ( $hs as $h )
     <tr>
-        <td>{{ $h->rev }}</td>
         <td><a href="/wiki/{{ $h->slug }}/{{ $h->rev }}">{{ $h->title }}</a></td>
         <td>{{ $h->created_at->diffForHumans() }}</td>
         <td><a href="/members/{{ $h->writer_id }}">{{ $h->writer->name }}</a></td>
