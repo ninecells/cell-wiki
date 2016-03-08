b93adbb5a3e01f6b3bb607f473811dcc6c62a5c5
koojunho <koojh@9cells.com>
2016-03-08 14:59:46 +0900

    탭 메뉴 순서 변경

--- resources/views/wiki/parts/top_tab.blade.php
+++ resources/views/wiki/parts/top_tab.blade.php
@@ -2,14 +2,14 @@
     <li role="presentation"{!! $type === 'view' ? ' class="active"' : '' !!}>
         <a href="/wiki/{{ $page->slug or $page->title }}" aria-controls="view" role="tab" data-toggle="tab">보기</a>
     </li>
-    @can('wiki-write')
-    <li role="presentation"{!! $type === 'edit' ? ' class="active"' : '' !!}>
-        <a href="/wiki/{{ $page->slug or $page->title }}/edit" aria-controls="edit" role="tab" data-toggle="tab">편집</a>
-    </li>
-    @endcan
     @if ($page->slug)
     <li role="presentation"{!! $type === 'history' ? ' class="active"' : '' !!}>
         <a href="/wiki/{{ $page->slug }}/history" aria-controls="history" role="tab" data-toggle="tab">역사</a>
     </li>
     @endif
+    @can('wiki-write')
+    <li role="presentation"{!! $type === 'edit' ? ' class="active"' : '' !!}>
+        <a href="/wiki/{{ $page->slug or $page->title }}/edit" aria-controls="edit" role="tab" data-toggle="tab">편집</a>
+    </li>
+    @endcan
 </ul>
