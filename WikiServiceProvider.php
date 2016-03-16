928f34c929182687160dd6244ccfc69edb1ccab3
koojunho <koojh@9cells.com>
2016-03-16 12:26:36 +0900

    사소한 수정

--- WikiServiceProvider.php
+++ WikiServiceProvider.php
@@ -5,7 +5,7 @@ namespace NineCells\Wiki;
 use App;
 use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Support\ServiceProvider;
-use NineCells\Auth\MemberServiceProvider;
+use NineCells\Member\MemberServiceProvider;
 use Mews\Purifier\PurifierServiceProvider;
 use NineCells\Admin\PackageList;
 
